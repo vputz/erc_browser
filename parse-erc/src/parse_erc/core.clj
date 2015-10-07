94d7da41baea06c468ce1e5164090d5a9945fbe9
vputz <vputz@nyx.net>
2015-10-07 19:40:44 +0100

    added topic browsing

--- parse-erc/src/parse_erc/core.clj
+++ parse-erc/src/parse_erc/core.clj
@@ -1,6 +1,7 @@
 (ns parse-erc.core
   (require [clojure.java.io :as io]
            [clojure.data.csv :as csv]
+           [clojure.edn :as edn]
            [clojure.data.json :as json]
            [clojure.string :as str]
            [clojure.pprint :refer :all]
@@ -9,8 +10,8 @@
 
 
 (defn as-rows [file]
-    (process (take 10000
-     (csv/read-csv file :separator \;))))
+  (process 
+   (csv/read-csv file :separator \;)))
 
 (defn domains [] ["PE1" "PE2" "PE3" "PE4" "PE5" "PE6" "PE7" "PE8" "PE9" "PE10"
                   "LS1" "LS2" "LS3" "LS4" "LS5" "LS6" "LS7" "LS8" "LS9"
@@ -62,7 +63,8 @@
       true "Undefined")))
 
 (defn parse-int [s]
-  (Integer/parseInt (re-find #"\A-?\d+" s)))
+  (try (Integer/parseInt (re-find #"\A-?\d+" s))
+       (catch NumberFormatException e -1)))
 
 (defn call-year [call]
   (parse-int (re-find #"\d\d\d\d" call)))
@@ -121,20 +123,39 @@
         (let [erc-projects (filter #(begins-with "ERC" (% :topics)) proj-rows)]
           (do 
             (let [projects (map #(let [participants (get orgs (:reference %))]
-                                   (do (println "BOOO" participants)
+                                   (do (if (= 0 (count participants))
+                                         (println "No participants for ref " (:reference %) ", acronym " (:acronym %)))
                                        (assoc % :participants participants))) erc-projects)] ;(:participants(first %) :participants (second %)) (map vector proj-rows org-rows))]
               (doall projects))))))))
 
 
+(defn corpus-entry 
+  "Make a corpus entry from a cordis entry"
+  [m]
+  (hash-map :abstract (clojure.string/replace (:objective m) (str \newline) " ")
+            :acronym (:acronym m)
+            :ec-contribution (parse-int (:ecMaxContribution m))
+            :reference (:reference m)))
+
 (defn abstract-corpus [proj-reader]
   (let [proj-rows (as-rows proj-reader)]
-    (filter (complement clojure.string/blank?)
-            (map #(clojure.string/replace (:objective %) (str \newline) " ") proj-rows))))
+    (filter #((complement clojure.string/blank?) (:abstract %))
+            (map corpus-entry proj-rows))))
+
+(defn take-csvrows [n filename]
+  (with-open [in-file (io/reader filename)]
+    (let [csv-rows (as-rows in-file)]
+      (doall (take n csv-rows)))))
 
 (defn write-corpus [proj-filename out-filename]
   (with-open [in-file (io/reader proj-filename)
               out-file (io/writer out-filename)]
-    (.write out-file (apply str (interpose (str \newline) (abstract-corpus in-file))))))
+    (.write out-file (apply str (interpose (str \newline) (map :abstract (abstract-corpus in-file)))))))
+
+(defn write-corpus-info [proj-filename out-filename]
+  (with-open [in-file (io/reader proj-filename)
+              out-file (io/writer out-filename)]
+    (json/write (abstract-corpus in-file) out-file)))
 
 (defn make-projects-json []
   (let [fp7-entries (erc-entries "cordis-fp7projects.csv" "cordis-fp7organizations.csv") 
